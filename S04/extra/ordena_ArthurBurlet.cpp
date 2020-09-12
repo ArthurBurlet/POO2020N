fd247b638216efd9c61d9511a2aac7191be27e21
Arthur Burlet <70483735+ArthurBurlet@users.noreply.github.com>
2020-09-12 10:19:35 -0300

    Update ordena_ArthurBurlet.cpp

--- S04/extra/ordena_ArthurBurlet.cpp
+++ S04/extra/ordena_ArthurBurlet.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 using namespace std; 
   
-void swap(int *x, int *y)  
+/*void swap(int *x, int *y)  
 {  
     int temp = *x;  
     *x = *y;  
     *y = temp;  
-}  
+}*/  
   
 void selectionSort(int vet[], int n)  
 {  
